52aeac03186ad7c020907a9dca3a047c32e019e5
Alan West <3676547+alanwest@users.noreply.github.com>
2022-10-19 18:35:59 -0700

    Initial commit

---
+++ MyApplication/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using OpenTelemetry;
+using OpenTelemetry.Trace;
+
+namespace MyApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var tracerProvider = Sdk.CreateTracerProviderBuilder()
+                .SetSampler(new AlwaysOnSampler())
+                .AddHttpClientInstrumentation()
+                .AddConsoleExporter()
+                .Build();
+
+            NetFrameworkLibrary.SomeClass.MakeAnHttpCall().GetAwaiter().GetResult();
+            NetStandardLibrary.SomeClass.MakeAnHttpCall().GetAwaiter().GetResult();
+
+            tracerProvider.Dispose();
+
+            Console.WriteLine("Press enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ NetStandardLibrary/SomeClass.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace NetStandardLibrary
+{
+    public class SomeClass
+    {
+        private static HttpClient HttpClient = new HttpClient();
+
+        public static async Task MakeAnHttpCall()
+        {
+            await HttpClient.GetStringAsync("https://opentelemetry.io");
+        }
+    }
+}
